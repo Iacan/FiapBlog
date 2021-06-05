91b340ede3452a5f0c41be9cbeda5061e22bc379
Iacan <iacan.ramos@gmail.com>
2021-06-04 22:13:27 -0300

    Adicionar arquivos de projeto.

---
+++ FiapBlog/Services/IBlogService.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace FiapBlog.Controllers
+{
+    public interface IBlogService
+    {
+        List<BlogPost> GetLatestPosts();
+        string GetPostText(string link);
+
+        List<BlogPost> GetOlderPosts(int oldestPostId);
+    }
+}
